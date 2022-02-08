acf54c03783ceed078bdf1f2cc0ede0985d96eb0
GeorgeBurns <lp21819@bristol.ac.uk>
2022-02-08 15:51:50 +0000

    added new figure

--- graph.py
+++ graph.py
@@ -4,9 +4,10 @@ import numpy as np
 
 x = [i for i in range(0,6)]
 y = [0,1,4,2,1,8]
+z = [0,1,2,2,2,2]
 plt.title("Confusion over time at uni")
 plt.xlabel("Months")
 plt.ylabel("Confusion")
-plt.plot(x,y)
+plt.plot(x,y,z)
 plt.show()
 
